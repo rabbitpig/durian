4b09810d5104f06f1022924c5c0cf69a05acc47b
rabbitpig <double_jia@qq.com>
2023-10-13 06:17:23 +0800

    add json value support

---
+++ src/main/java/org/rabbitpig/durian/mybatis/JsonValueTypeHandler.java
@@ -0,0 +1,76 @@
+package org.rabbitpig.durian.mybatis;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * JSON处理
+ *
+ * @param <E>
+ * @author ajia.zjj
+ */
+public class JsonValueTypeHandler<E extends Object> extends BaseTypeHandler<E> {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        // 反序列化时，兼容未映射字段
+        MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    private Class<E> clazz;
+
+    public JsonValueTypeHandler(Class<E> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Type argument cannot be null");
+        }
+        this.clazz = clazz;
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, this.toJson(parameter));
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return this.toObject(rs.getString(columnName), clazz);
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return this.toObject(rs.getString(columnIndex), clazz);
+    }
+
+    @Override
+    public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return this.toObject(cs.getString(columnIndex), clazz);
+    }
+
+    private String toJson(E object) {
+        try {
+            return MAPPER.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private E toObject(String content, Class<?> clazz) {
+        if (content != null && !content.isEmpty()) {
+            try {
+                return (E) MAPPER.readValue(content, clazz);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+}
